b3696e71a5eec60be41b74ade3f4e71e31fe817f
winters <jonwinters1992@gmail.com>
2017-01-03 15:52:53 +0800

    camke

---
+++ CMakeFiles/routedns.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/routedns.dir/routedns.cpp.o"
+  "CMakeFiles/routedns.dir/parsedns.cpp.o"
+  "routedns.pdb"
+  "routedns"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/routedns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
